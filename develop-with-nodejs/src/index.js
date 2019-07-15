cac578a10d66113e8426347998da148073f52a5e
zenbox <michael@zenbox.de>
2019-07-15 08:24:31 +0200

    Create nodejs boilerplate

--- develop-with-nodejs/src/index.js
+++ develop-with-nodejs/src/index.js
@@ -1,33 +1,39 @@
 /**
  * DOM Manipulation via JS
- * 
+ *
+ * @package webapp
  * @module example
- * @author Michael <michael.reichart@gfu.net>
- * @since 1.0.0
  * @version 1.0.0
+ * @since 1.0.0
+ * @author Michael <michael.reichart@gfu.net>
  * @copyright Michael Reichart 2019
  * @license MIT
  */
 
-// import './style.css';
+import "./sass/style.scss";
 
 /**
  * This is a function
- * 
+ *
  * @param {string} e a sample element tagname
- * @param {string} t a sample text 
+ * @param {string} t a sample text
  * @return {object}
  */
 function paragraph(_e, _t) {
-    const node = document.createElement(_e);
-    const text = document.createTextNode(_t);
+  const node = document.createElement(_e);
+  const text = document.createTextNode(_t);
 
-    node.appendChild(text);
-    node.classList.add('default')
+  node.appendChild(text);
+  node.classList.add("default");
 
-    return node;
+  return node;
 }
 
 document
-    .querySelector('main>article')
-    .appendChild(paragraph('p', 'Cras Javascript justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Etiam porta sem malesuada magna mollis euismod.'));
+  .querySelector("main>article")
+  .appendChild(
+    paragraph(
+      "p",
+      "Cras Javascript justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Etiam porta sem malesuada magna mollis euismod."
+    )
+  );
--- develop-with-nodejs/webpack.config.js
+++ develop-with-nodejs/webpack.config.js
@@ -1,8 +1,21 @@
+// webpack.config.js
+// npm run build
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const Fiber = require('fibers');
 
 module.exports = {
+    // - - - - -
+    // MODE
+    // - - - - -
     mode: 'development',
+    // mode: 'production',
 
+    // - - - - -
+    // WATCH
+    // - - - - -
     watch: true,
     watchOptions: {
         aggregateTimeout: 300,
@@ -10,32 +23,76 @@ module.exports = {
         ignored: /node_modules/
     },
 
-    entry: './src/index.ts',
+    // - - - - -
+    // ENTRY
+    // - - - - -
+    entry: {
+        main: './src/index.js'
+    },
 
+    // - - - - -
+    // OUTPUT
+    // - - - - -
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    // - - - - -
+    // TARGET & EXTERNALS
+    // - - - - -
+    target: 'node', // update from 23.12.2018
+    externals: [nodeExternals()], // update from 23.12.2018
+    // - - - - -
+    // MODULES
+    // - - - - -
     module: {
         rules: [{
-            test: [
-                /\.tsx?$/,
-                /\.css$/
-            ],
-            use: [
-                'ts-loader',
-                'css-loader'
-            ],
-            exclude: /node_modules/
-        }]
+                // Javascript Settings
+                test: /\.jsx?$/,
+                exclude: /node_modules/
+            },
+            {
+                // SASS Settings
+                test: /\.scss$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../css',
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    {
+                        loader: 'css-loader' //   translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'sass-loader', // compiles Sass to CSS, using Node Sass by default
+                    }
+                ]
+            }
+        ]
     },
 
     resolve: {
         extensions: [
-            '.tsx',
-            '.ts',
-            '.js'
+            '.js',
+            '.jsx',
+            '.scss'
         ]
     },
 
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/assets/js')
-    }
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
 };
